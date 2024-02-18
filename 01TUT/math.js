727a92c6b17a9ce24c86f471bf06a250ea021506
Maxim Tikhon <tikhon.maxim@gmail.com>
2024-02-18 17:02:52 +0100

    init

---
+++ 01TUT/math.js
@@ -0,0 +1,10 @@
+const add = (a, b) => a + b
+const substruct = (a, b) => a - b
+const multiply = (a, b) => a * b
+const divide = (a, b) => a / b
+
+module.exports = { add, substruct, multiply, divide }
+
+// another way
+//exports.add = (a, b) => a + b
+
