57fd0116cec65b7b4d01f87b3bcd21d20da319f6
Maxim Tikhon <tikhon.maxim@gmail.com>
2024-02-21 14:11:19 +0100

    create API

---
+++ 09TUT/server.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const express = require('express');
+const { logger } = require('./middleware/logEvents');
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
+const errorHandler = require('./middleware/errorHandler');
+
+const app = express();
+const PORT = process.env.PORT || 3500;
+// custom middleware logger
+app.use(logger);
+
+// Cross Origin Resource Sharing
+app.use(cors(corsOptions));
+
+// for public API
+// app.use(cors());
+
+// built-in middleware to handle urlencoded data form data
+app.use(express.urlencoded({ extended: false }));
+
+// built-in middleware for json 
+app.use(express.json());
+
+//serve static files
+app.use(express.static(path.join(__dirname, '/public')));
+
+// routes
+app.use('/', require('./routes/root'));
+app.use('/employees', require('./routes/api/employees'));
+
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ "error": "404 Not Found" });
+    } else {
+        res.type('txt').send("404 Not Found");
+    }
+});
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
